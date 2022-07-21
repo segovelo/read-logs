2384a5d44171f938707dc137c3a1b4ad9a77e317
segovelo <gauchosinpingo@gmail.com>
2022-07-21 21:47:15 +0100

    Addition of save o file functionality

--- read-logs.py
+++ read-logs.py
@@ -5,6 +5,7 @@ Created on Wed Jul 20 22:38:07 2022
 @author: Sebastian
 """
 from argparse import ArgumentParser
+import re
 
 def check_file(file_name):
     file_name = file_name.translate({ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
@@ -36,11 +37,14 @@ def read(logs_file, correlation_id):
     flag1=False
     flag2=True
     while i < len(list1) and flag2:
-        index= list1[i].find(correlation_id.casefold())
-        if (index > -1) or (flag1 and j<5):
+        index = list1[i].find(correlation_id.casefold())
+        found_id = re.search(correlation_id, list1[i], re.IGNORECASE)
+        if found_id or (flag1 and j<5):
+        #if (index > -1) or (flag1 and j<5):
             list2.append(list1[i])
-            flag1=True        
-            if index==-1:
+            flag1=True
+            if not found_id:
+            #if index==-1:
                 j=j+1
             else:
                 j=0
@@ -49,26 +53,28 @@ def read(logs_file, correlation_id):
         i=i+1    
     list3=list2[:len(list2)-j]
     print("\n\n")
-    print("\n".join(list3))     
- 
+    print("\n".join(list3))
+    return ("\n".join(list3))   
+
+def save(data, save_file):
+    save_file = check_file(save_file)
+    with open(save_file, 'w') as f:
+        f.write(data)
+        f.close()
 
 if __name__ == '__main__':
    parser = ArgumentParser(description='A command line tool for extract info from logs')
-   parser.add_argument('-r', '--read', action='store_true', help='read correlation-id from logs')
+   parser.add_argument('-s', '--save', action='store', help='read correlation-id from logs')
    parser.add_argument('-l', '--logs', action='store', help='logs file passed as args')
    parser.add_argument('-c', '--corr', action='store', help='correlation-id passed as args')
 
    args = parser.parse_args()
-   correlation_id = "" 
-   logs_file=""
-   if args.logs:
-       logs_file=args.logs
-       print("\n logs file passed as argument: % s" % args.logs)
+
    if args.corr:
-       correlation_id = args.corr
-       print("\n correlation-id passed as argument: % s" % args.corr)
-   if args.read:
-       read(logs_file, correlation_id)
+       print("\n correlation-id passed as argument: % s\n\n" % args.corr)
+   if args.logs:
+       print("\n logs file passed as argument: % s" % args.logs)
+       save(read(args.logs, args.corr), args.save)
    else:
        print('Use the -h or --help flags for help')
    
